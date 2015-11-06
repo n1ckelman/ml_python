33ec46482dd9267ddbf87c2d87587d8f315c7ef1
n1ckelman <rybkin.k@gmail.com>
2015-11-06 02:22:06 +0200

    first prob model

--- 1.py
+++ 1.py
@@ -8,6 +8,9 @@ y = data[:,1]
 x = x[~sp.isnan(y)]
 y = y[~sp.isnan(y)]
 
+fpl, residuals, rank, sv, rcond = sp.polyfit(x,y,1,full=True)
+fl = sp.poly1d(fpl)
+
 plt.scatter(x,y, s=10)
 plt.title("Web traffic over the last month")
 plt.xlabel("Time")
@@ -15,4 +18,7 @@ plt.ylabel("Hits/hour")
 plt.xticks([w*7*24 for w in range(10)], ['week %i' % w for w in range(10)])
 plt.autoscale(tight=True)
 plt.grid(True, linestyle='-', color='0.75')
+fx = sp.linspace(0,x[-1],1000)
+plt.plot(fx, fl(fx), linewidth=4)
+plt.legend(["d=%i" %fl.order], loc= "upper left")
 plt.show()
