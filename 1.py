2258fd7c090e603516b143f3bd3b8cf909043852
n1ckelman <rybkin.k@gmail.com>
2015-11-06 02:06:38 +0200

    added first example

---
+++ 1.py
@@ -0,0 +1,18 @@
+import scipy as sp
+import matplotlib.pyplot as plt
+
+data = sp.genfromtxt("C:\Users\Rybkin\Desktop\web_traffic.tsv", delimiter="\t")
+x = data[:,0]
+y = data[:,1]
+
+x = x[~sp.isnan(y)]
+y = y[~sp.isnan(y)]
+
+plt.scatter(x,y, s=10)
+plt.title("Web traffic over the last month")
+plt.xlabel("Time")
+plt.ylabel("Hits/hour")
+plt.xticks([w*7*24 for w in range(10)], ['week %i' % w for w in range(10)])
+plt.autoscale(tight=True)
+plt.grid(True, linestyle='-', color='0.75')
+plt.show()
